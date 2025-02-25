89a288359cfc51e9fd4a9dd92d449f2c75ad940c
JhonnyRamos04 <onlyjhonny01@gmail.com>
2025-02-25 19:06:54 -0400

    Se actualizo el proyecto, se instalo react y se modificaron los servicios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
   vite: {
     ssr: {
       noExternal: ["astro", "@astrojs/"],
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
@@ -12,9 +13,29 @@ module.exports = {
           ...defaultTheme.fontFamily.sans,
         ],
       },
+      colors: {
+        emerald: {
+          950: '#052e16',
+        }
+      },
       dropShadow: {
-				'white': '0 45px 65px rgba(102, 150, 217, 0.15)'
-			}
+        'white': '0 45px 65px rgba(102, 150, 217, 0.15)'
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
